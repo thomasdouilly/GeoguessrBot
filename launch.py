724f431d89776192a68f52d801153952d23cc604
thomasdouilly <119968854+thomasdouilly@users.noreply.github.com>
2023-04-12 20:43:24 +0200

    Deletion of leaflet.js + command update in launch.py

--- launch.py
+++ launch.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import requests
 
 subprocess.run('bash -c "conda activate geoestimation-github-pytorch; python -V"', shell=True)
 os.system('pip install django')
@@ -9,5 +10,7 @@ os.system('pip install keyboard')
 os.system('pip install mouse')
 os.system('pip install pyautogui')
 os.system('pip install chardet')
+
+open('leaflet.js', 'wb').write(requests.get('https://unpkg.com/leaflet@1.9.3/dist/leaflet.js', allow_redirects=True).content)
 
 os.system('python ./backend_geoguessr/manage.py runserver 8000')
